0d164ef6d4592111bab8c9e83df94a973dbf6107
Krzysztof Bielecki <krzysztof-bielecki@wp.pl>
2019-07-18 17:20:03 +0200

    commit

--- run_analysis.R
+++ run_analysis.R
@@ -8,7 +8,15 @@ run_analysis<-function(path="./UCI HAR Dataset") {
         tbl1<-tbl_df(df1)
         
         df2<-read.table("./features.txt",stringsAsFactors = FALSE)
-        names(tbl1) <-df2$V2
+        
+        # names(tbl1) <-df2$V2
+        # some columns have the same feature name so i expand variable name with ordinal number from features.txt file!!!
+        names(tbl1) <-paste0(df2$V2,"_",df2$V1)
+        
+        
+        # select apprioprate variables
+        # and expand dataset with "test" flag to indicate file origin
+        tbl1<-tbl1 %>% select(matches("mean|std")) %>% mutate(file="test")
         
         print(tbl1)
         names(tbl1)
